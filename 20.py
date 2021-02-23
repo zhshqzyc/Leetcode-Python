7101a4138c5a878d70b3aa25bf0501f2a14105c9
Hui Zhao <zhshqzyc@gmail.com>
2021-02-23 09:48:49 -0500

    Create 20.py

---
+++ 20.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        lookup = {"(": ")", "{": "}", "[": "]"}
+        for parenthese in s:
+            if parenthese in lookup:
+                stack.append(parenthese)
+            elif len(stack) == 0 or lookup[stack.pop()] != parenthese:
+                return False
+        return len(stack) == 0
+
+s = Solution()
+print(s.isValid("()[]{}"))        
+
